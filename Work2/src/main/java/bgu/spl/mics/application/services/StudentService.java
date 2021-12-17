6fa9d5897c882bc40796c6222f861cebd4623f6c
liad-ahrak <liadah@post.bgu.ac.il>
2021-12-17 19:55:13 +0200

    17.12 1955

--- Work2/src/main/java/bgu/spl/mics/application/services/StudentService.java
+++ Work2/src/main/java/bgu/spl/mics/application/services/StudentService.java
@@ -93,6 +93,7 @@ public class StudentService extends MicroService {
                 if(student == model.getStudent())
                     publications++;
             }
+            student.addPublications(publications);
         });
     }
 }
